055c7f316d82f34726baf7d8bf91596762d01a5b
OmiKat <ompraveer@gmail.com>
2025-03-05 04:35:20 +0530

    adding files

---
+++ src/main/java/com/example/QuizMicroservice/Controller/QuizController.java
@@ -0,0 +1,34 @@
+package com.example.QuizMicroservice.Controller;
+
+
+import com.example.QuizMicroservice.Model.QuestionWrapper;
+import com.example.QuizMicroservice.Model.Response;
+import com.example.QuizMicroservice.Service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category , @RequestParam int numQ , @RequestParam String title){
+        return quizService.createQuiz(category,numQ,title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable int id){
+        return quizService.getQuiz(id);
+    }
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> getResponse(@PathVariable Integer id , @RequestBody List<Response> responses){
+        return quizService.calculateResult(id,responses);
+    }
+
+}
